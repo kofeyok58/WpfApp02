85354def055a4832c70ee6bf81ea010b18f214c0
kofeyok58 <zlatakupnevich@gmail.com>
2024-11-27 21:55:33 +1000

    Add project files.

---
+++ WpfApp6/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 window1 = new Window1();
+            window1.Show();
+            this.Close();
+        }
+
+        private void radiobtn1_Checked(object sender, RoutedEventArgs e)
+        {
+            
+            Application.Current.Resources["MyImageSource"] = new BitmapImage(new Uri(@"C:\Users\user\Downloads\image0.jpg"));
+        }
+
+        private void radiobtn2_Checked(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Resources["MyImageSource"] = new BitmapImage(new Uri(@"C:\Users\user\Downloads\cat2.jpg"));
+
+        }
+
+        private void radiobtn3_Checked(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Resources["MyImageSource"] = new BitmapImage(new Uri(@"C:\Users\user\Downloads\i.webp"));
+
+        }
+    }
+}
---
+++ WpfApp6/Window1.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp6
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        private Random random = new Random();
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            byte r = (byte)random.Next(256);
+            byte g = (byte)random.Next(256);
+            byte b = (byte)random.Next(256);
+
+            grid1.Background = new SolidColorBrush(Color.FromRgb(r, g, b));
+        }
+    }
+}
